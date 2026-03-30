a65245fbc724e89c1e9e1b588290f3ed67d7d45f
Bishnu Mishra <mishravishnu369@gmail.com>
2026-03-31 00:50:41 +0530

    interpolation

--- src/app/components/data-binding/data-binding.ts
+++ src/app/components/data-binding/data-binding.ts
@@ -6,4 +6,29 @@ import { Component } from '@angular/core';
   templateUrl: './data-binding.html',
   styleUrl: './data-binding.css',
 })
-export class DataBinding {}
+export class DataBinding {
+  courseName: string = "Angular Data Binding";
+  currentDate: Date = new Date();
+  rollNo: number = 101;
+
+  displayCourseInfo() {
+    console.log(`Course Name: ${this.courseName}`);
+    console.log(`Current Date: ${this.currentDate}`);
+    console.log(`Roll Number: ${this.rollNo}`);
+  }
+
+  displayCourseInfoForUI() {
+    return `Course Name: ${this.courseName},
+    Current Date: ${this.currentDate.toLocaleDateString()},
+    Roll Number: ${this.rollNo}`;
+  }
+
+
+  constructor() {
+    // Accessing a variable
+    this.courseName = "Updated Course Name";
+
+    // Accessing a method
+    this.displayCourseInfo();
+  }
+}
