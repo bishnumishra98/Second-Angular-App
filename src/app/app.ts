de684538d11d8df15fc835de4336eddaafc7af7b
Bishnu Mishra <mishravishnu369@gmail.com>
2026-04-07 02:10:27 +0530

    added RouterLink and RouterLinkActive

--- src/app/app.ts
+++ src/app/app.ts
@@ -10,13 +10,22 @@
 // tag where you want the routed components to be displayed.
 // And then you can define your routes in a separate file (e.g., app.routes.ts) and use the
 // RouterModule to configure the routes in your application.
+//
+// RouterLink is a directive that allows you to link to specific routes in your application.
+// It is used to create navigation links in your templates. When a user clicks on a RouterLink,
+// it will navigate to the specified route and load the corresponding component.
+//
+// RouterLinkActive is a directive that adds a CSS class to an element when the linked route is active.
+// It is used to highlight the active navigation link in your templates. When a user clicks on a
+// RouterLink, the RouterLinkActive directive will add the specified CSS class to the element,
+// allowing you to style it differently when it is active.
 
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RouterLink, RouterLinkActive],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
