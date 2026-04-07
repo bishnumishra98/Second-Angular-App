3a5b800b38cddecd8f62ac906fb3dfa20bba9ae6
Bishnu Mishra <mishravishnu369@gmail.com>
2026-04-08 02:56:51 +0530

    MAde POST API

--- src/app/components/photos/photos.ts
+++ src/app/components/photos/photos.ts
@@ -30,13 +30,22 @@ export class Photos {
   }
 
   
-
+  // GET method to fetch all photos from the API endpoint
   getAllPhotos() {
     // Make an HTTP GET request to the specified URL and subscribe to the response.
     // subscribe() is used to handle the asynchronous response from the HTTP request.
     // It takes a callback function that will be executed when the response is received.
     this.http.get("https://jsonplaceholder.typicode.com/photos").subscribe((response: any) => {
       this.photosList = response;   // assign the response data to the photosList property
+    });
+  }
+
+  // POST method to save a new photo to the API endpoint
+  onSavePhoto() {
+    // Make an HTTP POST request to the specified URL with the newPhoto data and subscribe to the response.
+    this.http.post("https://jsonplaceholder.typicode.com/photos", this.newPhoto).subscribe((response: any) => {
+      console.log("Photo saved successfully:", response);   // log the response to the console
+      this.getAllPhotos();   // refresh the photos list after saving a new photo
     });
   }
 }
